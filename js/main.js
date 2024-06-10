767070638894b13763a4e7a9d7cf7c2cdc7eea4b
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-06-10 20:18:37 +0530

    professions section modal js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
+//scroll down
 window.addEventListener("scroll", function(){
     const header = this.document.querySelector("header");
     header.classList.toggle("sticky", this.window.scrollY > 0);
+});
+
+//professions section - modal
+const professionsModals = document.querySelectorAll(".profession-modal");
+const professionsBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    professionsModals[modalClick].classList.add("active");
+}
+
+learnMoreBtns.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        professionsModals.forEach((professionView) => {
+            modalView.classList.remove("active");
+        });
+    });
 });
