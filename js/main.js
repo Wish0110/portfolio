8c5c2f153c40695d6ce869cdfa4d91aa06ebbb1b
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-06-17 21:13:24 +0530

    scroll up js

--- js/main.js
+++ js/main.js
@@ -49,3 +49,15 @@ projectCloseBtns.forEach((projectCloseBtn) => {
         });
     });
 });
+
+//scrollup 
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
