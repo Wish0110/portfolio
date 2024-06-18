3db13d11eff5d46f45941ffe130d986584559882
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-06-18 20:21:34 +0530

    responsive nave bar

--- js/main.js
+++ js/main.js
@@ -109,5 +109,9 @@ const closeBtn = document.querySelector(".nav-close-btn");
 const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
     navigation.classList.remove("active");
 });
